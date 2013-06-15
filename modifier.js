2d83f829bf07f8973363c0b105f7ac781e6708dd
Jesse Luo <radishchrist@gmail.com>
2013-06-16 04:24:38 +0800

    added support of tangcha

--- modifier.js
+++ modifier.js
@@ -1,5 +1,36 @@
 function processTangcha (data, status) {
-	console.log(data);
+	var _start, _end;
+	var _link, _price;
+
+	if (status === "success") {
+		if (data.indexOf("无法找到相关的书籍") == -1) {
+			_start = data.indexOf("book-cell");
+			_start = data.indexOf("href", _start) + 7;
+			_end = data.indexOf("class", _start) - 2;
+			_link = data.slice(_start, _end);
+			_link = "http://tangcha.tc/" + _link;
+
+			// _start = data.indexOf("price\">") + 15;
+			// _end = data.indexOf("<", _start);
+			// _price = "RMB " + data.slice(_start, _end);
+			_price = "RMB ??";
+
+			console.log(_link);
+			console.log(_price);
+
+			var _itemNode = stdInfoNode.cloneNode(true);
+			_itemNode.getElementsByTagName("a")[0].href=_link;
+			_itemNode.getElementsByTagName("span")[0].firstChild.nodeValue="字节社";
+			_itemNode.getElementsByTagName("span")[1].getElementsByTagName("span")[0].firstChild.nodeValue=_price;
+			buyinfoOfEbook.getElementsByTagName("ul")[0].appendChild(_itemNode);
+		}
+		else{
+			console.log("No found on Tangcha");
+		}
+	}
+	else {
+		console.log(status);
+	}
 }
 
 function processDuokan (data, status) {
@@ -17,17 +48,21 @@ function processDuokan (data, status) {
 			_start = data.indexOf("price\">") + 15;
 			_end = data.indexOf("<", _start);
 			_price = "RMB " + data.slice(_start, _end);
+
+			console.log(_link);
+			console.log(_price);
+
+			var _itemNode = stdInfoNode.cloneNode(true);
+			_itemNode.getElementsByTagName("a")[0].href=_link;
+			_itemNode.getElementsByTagName("span")[0].firstChild.nodeValue="多看";
+			_itemNode.getElementsByTagName("span")[1].getElementsByTagName("span")[0].firstChild.nodeValue=_price;
+			buyinfoOfEbook.getElementsByTagName("ul")[0].appendChild(_itemNode);
+			console.log(_itemNode);
+		}
+		else{
+			console.log("No found on Duokan");
 		}
 
-		console.log(_link);
-		console.log(_price);
-
-		var _itemNode = stdInfoNode.cloneNode(true);
-		_itemNode.getElementsByTagName("a")[0].href=_link;
-		_itemNode.getElementsByTagName("span")[0].firstChild.nodeValue="多看";
-		_itemNode.getElementsByTagName("span")[1].getElementsByTagName("span")[0].firstChild.nodeValue=_price;
-		buyinfoOfEbook.getElementsByTagName("ul")[0].appendChild(_itemNode);
-		console.log(_itemNode);
 	}
 	else {
 		console.log(status);
@@ -44,6 +79,7 @@ $(document).ready(function() {
 	var _bookname = document.title.slice(0, document.title.length - 5);
 	var _bookstores = [];
 	_bookstores[0] = new OnlineBookStore("Duokan", "http://book.duokan.com/search/{{=bookname }}/1", processDuokan);
+	_bookstores[1] = new OnlineBookStore("Tangcha", "http://tangcha.tc/books/search/{{=bookname }}", processTangcha);
 
 	for (var i = 0; i < _bookstores.length; i++) {
 		console.log(i);
@@ -70,7 +106,6 @@ $(document).ready(function() {
 		for (i = 0; i < _numOfBuyinfoItemNodes; i++) {
 			_buyinfoItemNodes[0].parentNode.removeChild(_buyinfoItemNodes[0]);
 		}
-		// $("#buyinfo-printed").before(buyinfoOfEbook);
 		buyinfoOfPrinted.parentNode.insertBefore(buyinfoOfEbook, buyinfoOfPrinted);
 	}
 	else
